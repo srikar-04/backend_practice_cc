b910c5e28ceb9ee9c98a5a9276e7d221fb8c08df
srikar <srikar.code01@gmail.com>
2025-01-21 22:05:20 +0530

    fix: correct syntax error in userSchema pre-save hook

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -40,7 +40,7 @@ const userSchema = new Schema({
     },
 }, {timestamps: true})
 
-userSchema.pre("save", async function {
+userSchema.pre("save", async function() {
     if(!this.isModified("password")) return next()
 
     this.password = await bcrypt(this.password, 10)

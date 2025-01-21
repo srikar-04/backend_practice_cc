6d5a3114674ad1e531377a453ca998cb92bf8112
srikar <srikar.code01@gmail.com>
2025-01-21 22:03:35 +0530

    feat: add user model with authentication methods and token generation

---
+++ src/models/user.models.js
@@ -0,0 +1,81 @@
+import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    coverImage: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: [true, 'password is a required field']
+    },
+    refreshToken: {
+        type: String
+    },
+}, {timestamps: true})
+
+userSchema.pre("save", async function {
+    if(!this.isModified("password")) return next()
+
+    this.password = await bcrypt(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(this.password, password)
+}
+
+userSchema.generateAcessToken = async function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.generateRefreshToken = async function()  {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
